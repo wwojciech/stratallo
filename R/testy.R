490a41515c96f007078ec267b23bc73944c6be03
Wojtek <wojciech.wojciak@contractors.roche.com>
2021-11-28 22:43:40 +0100

    Add box constraints + data_mM + tests

---
+++ R/testy.R
@@ -0,0 +1,52 @@
+noptcond_sufficient(
+  d = data_mM[, "N"] * data_mM[, "N"],
+  l = data_mM[, "m"],
+  u = data_mM[, "M"],
+  n = 2000
+)
+
+rNa(
+  d = data_mM[, "N"] * data_mM[, "N"],
+  m = data_mM[, "m"],
+  M = data_mM[, "M"],
+  n = 2000
+)
+
+sum(data_mM[, "m"]) # n min
+sum(data_mM[, "M"]) # n max
+# 1073+1 - 33471-1
+
+
+all(sapply(1074:33470, function(n) {
+  ggm <- noptcond_sufficient(
+    d = data_mM[, "N"] * data_mM[, "N"],
+    l = data_mM[, "m"],
+    u = data_mM[, "M"],
+    n = 2000
+  )
+  rna <- rNa(
+    d = data_mM[, "N"] * data_mM[, "N"],
+    m = data_mM[, "m"],
+    M = data_mM[, "M"],
+    n = 2000
+  )
+  all.equal(ggm, rna)
+}))
+
+
+bench::mark(
+  noptcond_sufficient(
+    d = data_mM[, "N"] * data_mM[, "N"],
+    l = data_mM[, "m"],
+    u = data_mM[, "M"],
+    n = 2000
+  ),
+  rNa(
+    d = data_mM[, "N"] * data_mM[, "N"],
+    m = data_mM[, "m"],
+    M = data_mM[, "M"],
+    n = 2000
+  ),
+  check = TRUE,
+  iterations = 10
+)[1:6]
