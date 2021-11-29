536f46587574d74e4f4a7719422fe90c1f00cde3
Wojciech Wójciak <11532997+wwojciech@users.noreply.github.com>
2021-11-29 15:38:55 +0100

    Update testy.R

--- R/testy.R
+++ R/testy.R
@@ -1,30 +1,33 @@
-# dane data_mM jest w katalogu ./data
+# zbior data_mM jest w katalogu ./data
 # load(file = "./data/data_mM.Rdata")
 
 d <- data_mM[, "N"] * data_mM[, "S"]
 m <- data_mM[, "m"]
 M <- data_mM[, "M"]
-sum(m) # n min
-sum(M) # n max
+sum(m) # minimalne feasible n
+sum(M) # maksymalne feasible n
 # 1073+1 - 3347-1
 
 # np. dla n = 2000
-noptcond_sufficient(d = d, l = m, u = M, n = 2000)
-rNa_mM(d = d, m = m, M = M, n = 2000)
+n <- 2000
+noptcond_sufficient(d = d, l = m, u = M, n = n)
+rNa_mM(d = d, m = m, M = M, n = n)
 
 bench::mark(
-  noptcond_sufficient(d = d, l = m, u = M, n = 2000),
-  rNa_mM(d = d, m = m, M = M, n = 2000),
+  noptcond_sufficient(d = d, l = m, u = M, n = n),
+  rNa_mM(d = d, m = m, M = M, n = n),
   check = TRUE,
   iterations = 10
 )[1:6]
 
 # w funcji n od 1074 do 3346
-all(sapply(1074:3346, function(n) {
-  all.equal(
-    noptcond_sufficient(d = d, l = m, u = M, n = n),
-    rNa_mM(d = d, m = m, M = M, n = n)
+all(
+  sapply(1074:3346, function(n)
+    all.equal(
+      noptcond_sufficient(d = d, l = m, u = M, n = n),
+      rNa_mM(d = d, m = m, M = M, n = n)
+    )
   )
-}))
+)
 
 
