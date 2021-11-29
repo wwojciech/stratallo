eceecefd7f6235499de558f7aac0f03ccf488e7a
Wojtek <wojciech.wojciak@contractors.roche.com>
2021-11-29 12:02:34 +0100

    Tests update

--- R/testy.R
+++ R/testy.R
@@ -1,52 +1,27 @@
-noptcond_sufficient(
-  d = data_mM[, "N"] * data_mM[, "N"],
-  l = data_mM[, "m"],
-  u = data_mM[, "M"],
-  n = 2000
-)
+d <- data_mM[, "N"] * data_mM[, "S"]
+m <- data_mM[, "m"]
+M <- data_mM[, "M"]
+sum(m) # n min
+sum(M) # n max
+# 1073+1 - 3347-1
 
-rNa_mM(
-  d = data_mM[, "N"] * data_mM[, "N"],
-  m = data_mM[, "m"],
-  M = data_mM[, "M"],
-  n = 2000
-)
-
-sum(data_mM[, "m"]) # n min
-sum(data_mM[, "M"]) # n max
-# 1073+1 - 33471-1
-
-
-all(sapply(1074:33470, function(n) {
-  ggm <- noptcond_sufficient(
-    d = data_mM[, "N"] * data_mM[, "N"],
-    l = data_mM[, "m"],
-    u = data_mM[, "M"],
-    n = 2000
-  )
-  rna <- rNa_mM(
-    d = data_mM[, "N"] * data_mM[, "N"],
-    m = data_mM[, "m"],
-    M = data_mM[, "M"],
-    n = 2000
-  )
-  all.equal(ggm, rna)
-}))
-
+# np. dla n = 2000
+noptcond_sufficient(d = d, l = m, u = M, n = 2000)
+rNa_mM(d = d, m = m, M = M, n = 2000)
 
 bench::mark(
-  noptcond_sufficient(
-    d = data_mM[, "N"] * data_mM[, "N"],
-    l = data_mM[, "m"],
-    u = data_mM[, "M"],
-    n = 2000
-  ),
-  rNa_mM(
-    d = data_mM[, "N"] * data_mM[, "N"],
-    m = data_mM[, "m"],
-    M = data_mM[, "M"],
-    n = 2000
-  ),
+  noptcond_sufficient(d = d, l = m, u = M, n = 2000),
+  rNa_mM(d = d, m = m, M = M, n = 2000),
   check = TRUE,
   iterations = 10
 )[1:6]
+
+# w funcji n od 1074 do 3346
+all(sapply(1074:3346, function(n) {
+  all.equal(
+    noptcond_sufficient(d = d, l = m, u = M, n = n),
+    rNa_mM(d = d, m = m, M = M, n = n)
+  )
+}))
+
+
