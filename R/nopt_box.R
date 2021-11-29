0071c598938d5a8f1a2c34ea83b3cf6b89e061bb
Wojciech Wójciak <11532997+wwojciech@users.noreply.github.com>
2021-11-29 22:48:42 +0100

    Update nopt_box.R

--- R/nopt_box.R
+++ R/nopt_box.R
@@ -8,7 +8,7 @@ rNa_mM <- function(d, m, M, n, verbose = FALSE) {
   repeat {
     nopt_candidate <- rNa(d[W], M[W], n) # step 1
     A <- which(nopt_candidate <= m[W]) # step 2
-    if (length(A) == 0)
+    if (length(A) == 0) # step 3
       break
     else {
       n <- n - sum(m[W[A]])
