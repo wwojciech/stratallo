922a118c20d827d2037a285634ad1562c9f4b2d1
Wojciech Wójciak <11532997+wwojciech@users.noreply.github.com>
2021-11-29 15:51:46 +0100

    Update testy.R

--- R/testy.R
+++ R/testy.R
@@ -1,25 +1,28 @@
+#################
+# dane
+#################
+
 # zbior data_mM jest w katalogu ./data
 # load(file = "./data/data_mM.Rdata")
 
 d <- data_mM[, "N"] * data_mM[, "S"]
 m <- data_mM[, "m"]
 M <- data_mM[, "M"]
-c(sum(m), sum(M)) # (minimalne, maksymalne) feasible n
+n_feasible <- (sum(m)+1):(sum(M)-1) # wektor feasible n (z wylaczeniem dwoch trywialnych przypadkow)
 # 1073; 3347
 
 #################
 # poprawnosc
 #################
 
-# np. dla n = 2000
-n <- 2000
+# n - skalar
+n <- sample(n_feasible, size = 1)
 noptcond_sufficient(d = d, l = m, u = M, n = n)
 rNa_mM(d = d, m = m, M = M, n = n)
 
-# w funcji n, ktore przebiega od 1074 do 3346
-n <- (sum(m)+1):(sum(M)-1)
+# w funcji n, ktore przebiega przez wszystkie n_feasible
 all(
-  sapply(n, function(n_i)
+  sapply(n_feasible, function(n_i)
     all.equal(
       noptcond_sufficient(d = d, l = m, u = M, n = n_i),
       rNa_mM(d = d, m = m, M = M, n = n_i)
@@ -31,8 +34,8 @@ all(
 # czasy obliczen
 #################
 
-# np. dla n = 2000
-n <- 2000
+# n - skalar
+n <- sample(n_feasible, size = 1)
 bench::mark(
   noptcond_sufficient(d = d, l = m, u = M, n = n),
   rNa_mM(d = d, m = m, M = M, n = n),
