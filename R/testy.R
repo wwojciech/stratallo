669180b37abda0ff168ec913c7239315b46e7595
Wojciech Wójciak <11532997+wwojciech@users.noreply.github.com>
2021-11-29 15:43:22 +0100

    Update testy.R

--- R/testy.R
+++ R/testy.R
@@ -7,17 +7,15 @@ M <- data_mM[, "M"]
 c(sum(m), sum(M)) # (minimalne, maksymalne) feasible n
 # 1073; 3347
 
+###############
+# poprawnosc
+###############
+
 # np. dla n = 2000
 n <- 2000
 noptcond_sufficient(d = d, l = m, u = M, n = n)
 rNa_mM(d = d, m = m, M = M, n = n)
 
-bench::mark(
-  noptcond_sufficient(d = d, l = m, u = M, n = n),
-  rNa_mM(d = d, m = m, M = M, n = n),
-  check = TRUE,
-  iterations = 10
-)[1:6]
 
 # w funcji n, ktore przebiega od 1074 do 3346
 n <- (sum(m)+1):(sum(M)-1)
@@ -29,3 +27,14 @@ all(
     )
   )
 )
+
+###############
+# czasy obliczen
+###############
+bench::mark(
+  noptcond_sufficient(d = d, l = m, u = M, n = n),
+  rNa_mM(d = d, m = m, M = M, n = n),
+  check = TRUE,
+  iterations = 10
+)[1:6]
+
